666a7695de207378c99ea2480c47dc9704ea8775
GLAUCIA <GLAUCIA@DESKTOP-SC4EM8U>
2017-11-18 13:46:35 -0200

    Meu primeior exemplo de commit dentro do netbeans

---
+++ src/exemplo/git/netbeans/ExemploGitNetbeans.java
@@ -0,0 +1,15 @@
+package exemplo.git.netbeans;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author GLAUCIA
+ */
+public class ExemploGitNetbeans {
+    Scanner scan = new Scanner (System.in);
+    public static void main(String[] args) {
+        System.out.println("Exemplo de um projeto com Git e Netbeans");    
+    }
+    
+}
